f6a27cf5a1f7725d39a791c6ef3c35dc5d477bdf
rebal2023 <rebal.wehbe@gmail.com>
2024-01-28 13:14:37 -0800

    6-7

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    for j in range(x + 1, 10):
+    for j in range(i + 1, 10):
         if x < 8:
-            print('{}{}, '.format(x, y), end='')
+            print('{}{}, '.format(i, j), end='')
         else:
             print('89')
