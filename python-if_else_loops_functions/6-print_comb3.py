8626c3e76234b7979419feb32c9e8f17de566414
rebal2023 <rebal.wehbe@gmail.com>
2024-01-28 13:12:42 -0800

    6-7

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(x + 1, 10):
+        if x < 8:
+            print('{}{}, '.format(x, y), end='')
+        else:
+            print('89')
