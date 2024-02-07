bc7786d11906d6941a95c06e6084751d5dde97e2
rebal2023 <rebal.wehbe@gmail.com>
2024-02-07 14:24:21 -0800

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This class defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size=0):
+        """Private instance attribute named size"""
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+        """Return the current square area"""
+        return (self.__size * self.__size)
---
+++ python-classes/3-square.py~
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This class defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size=0):
+        """Private instance attribute named size"""
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
