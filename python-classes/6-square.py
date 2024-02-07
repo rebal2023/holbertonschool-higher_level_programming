bfc4dac52af87897effb0bb74205101bd54a6762
rebal2023 <rebal.wehbe@gmail.com>
2024-02-07 14:51:34 -0800

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,11 +7,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Private instance attribute named size"""
-        
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
         
