9000e225e69708544d480c09f301add988a7e0ca
rebal2023 <rebal.wehbe@gmail.com>
2024-02-07 13:32:36 -0800

    1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This class defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """Private instance attribute named size"""
+        
+        self.__size = size
