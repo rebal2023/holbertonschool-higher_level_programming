a58488cdd052c6906361fbe75bb4e7c6e733d20a
rebal2023 <rebal.wehbe@gmail.com>
2024-02-11 14:01:08 -0800

    9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -61,6 +61,20 @@ class Rectangle:
                 rectangle += '\n'
         return rectangle
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
+
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
     @classmethod
     def square(cls, size=0):
         """returns a new Rectangle instance with width and height equal to size"""
@@ -76,28 +90,15 @@ class Rectangle:
             [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
-        return ("".join(rect)
+        return ("".join(rect))
     
     def __repr__(self):
         """return a string representation of the rectangle"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+    
     def __del__(self):
         """Print a message when an instance of Rectangle is deleted"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """returns the biggest rectangle based on the area"""
-        
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-    
