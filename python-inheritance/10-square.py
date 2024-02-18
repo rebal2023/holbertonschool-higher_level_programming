45b18a78fdbc33bab67d6778fca5d3c021fdd6bd
rebal2023 <rebal.wehbe@gmail.com>
2024-02-18 13:58:39 -0800

    10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Subclass representing a rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+        
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method for area of square"""
+        return self.__size ** 2
