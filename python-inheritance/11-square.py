948832a886ecd72ff473bc27dd4ac686d31ca789
rebal2023 <rebal.wehbe@gmail.com>
2024-02-20 13:23:19 -0800

    11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Subclass representing a rectangle"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+        
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Method for area of square"""
+        return self.__size ** 2
+
+     def __str__(self):
+        """ return the square description"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
