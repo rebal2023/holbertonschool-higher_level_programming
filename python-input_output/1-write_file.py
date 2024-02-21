d53572779f6fcdcb8919dcc1a6db46534f36a8ea
rebal2023 <rebal.wehbe@gmail.com>
2024-02-20 16:03:41 -0800

    1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that writes a
+string to a text file (UTF8)
+and returns the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Write a string to a text file (UTF8) and return the
+    number of characters written
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        count = file.write(text)
+
+    return count
