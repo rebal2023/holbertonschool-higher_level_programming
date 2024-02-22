ea9f1d27d70b69b61f8b05bca83d4989b133939e
rebal2023 <rebal.wehbe@gmail.com>
2024-02-22 01:45:52 +0000

    8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines class-to-JSON function"""
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure"""
+    return obj.__dict__
