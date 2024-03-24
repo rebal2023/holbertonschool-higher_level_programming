906bf7fdc6a083bc9d3cad6589012356257e893f
rebal2023 <rebal.wehbe@gmail.com>
2024-03-24 21:44:16 +0000

    4

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- A script that create the table id_not_null in MySQL Server with an ID default value
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256) NOT NULL
+);
